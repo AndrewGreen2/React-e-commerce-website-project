34f964f97a60a8bfee874d057ccdced11cc46f48
Andrew <atiddlytortoise@gmail.com>
2021-05-17 17:22:02 +1000

    Added working navbar & shopping cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react'
-
-//import Products from './components/Products/Products';
-//import Navbar from './components/Navbar/Navbar';
-import { Products, Navbar } from './components';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
+    const [products, setProducts] = useState([]); 
+    const [cart, setCart] = useState({});
+    
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
+    useEffect (() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    console.log(cart);
+        
     return (
         <div>
-           <Navbar />
-           <Products />
+           <Navbar totalItems={cart.total_items} />
+           {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+           <Cart cart={cart} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: "Shoes", description: 'Running Shoes.', price:'$5.00', image: 'https://www.target.com.au/medias/static_content/product/images/full/07/18/A1390718.jpg?impolicy=product_portrait_hero'},
-    {id: 2, name: "Macbook", description: 'Apple Macbook.',price:'$15.00', image: 'https://cdn.pocket-lint.com/r/s/970x/assets/images/152137-laptops-review-apple-macbook-pro-2020-review-image1-pbzm4ejvvs.jpg'},
-]
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
     return (
@@ -18,7 +14,7 @@ const Products = () => {
         <Grid container justify = "center" spacing = {4}>
             {products.map((product) => (
                 <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
+                    <Product product={product} onAddToCart={onAddToCart} />
                 </Grid>
             ))}
         </Grid>
